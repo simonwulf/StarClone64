aec516605c8e0536d7f45b9c729259f2fd6e6eaa
Master Kenth <MasterKenth@dafuqstudios.com>
2013-12-06 13:19:57 +0100

    Intial libraries
    Included libraries:
    
    GLEW
    GLFW
    SOIL
    
    Updated main.cpp to test functionality. Opens an empty OpenGL Context
    window.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,38 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GL/wglew.h>
+#include <gl/GLU.h>
+#include <GLFW/glfw3.h>
+#include <SOIL/SOIL.h>
 
-int main(int argc, const char* argv[]) {
+int main(int argc, const char* argv[])
+{
+	GLFWwindow* window;
+
+	glewExperimental = true;
+
+	if(!glfwInit())
+		return -1;
+
+	glfwWindowHint(GLFW_RESIZABLE, 0);
+	window = glfwCreateWindow(1280, 720, "OpenGL Window", NULL/*glfwGetPrimaryMonitor()*/, NULL);
+	glfwMakeContextCurrent(window);
+
+	glewInit();
+
+	std::cout << "GLEW version " << glewGetString(GLEW_VERSION) << "\n";
+
+	/* Test loop */
+	while(!glfwWindowShouldClose(window))
+	{
+		
+
+		glfwPollEvents();
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 
 	return 0;
 }
