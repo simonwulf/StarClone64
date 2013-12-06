159b9072f8a130e054b7f3da1b1e030f03458607
Master Kenth <MasterKenth@dafuqstudios.com>
2013-12-06 16:29:10 +0100

    Log Manager (unf)
    Started log manager. Logging in color works, but not logging to file
    yet.

---
+++ LogManager.cpp
@@ -0,0 +1,126 @@
+#include "LogManager.h"
+
+#include <iostream>
+#include <sstream>
+#include <ctime>
+#include <direct.h>
+
+LogManager LogManager::m_instance;
+
+LogManager::LogManager()
+{
+	bDisableFile = false;
+	m_hstdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	m_logFilePath = "logs\\";
+	m_logFileName = GenerateFileName();
+	VerifyLogPath();
+
+	if(!bDisableFile)
+		OpenFile();
+}
+
+LogManager::~LogManager()
+{
+	if(!bDisableFile)
+		CloseFile();
+}
+
+bool LogManager::Write( std::string text, int color )
+{
+	WriteCmd(text, color);
+	return (bDisableFile || WriteFile(text));
+}
+
+void LogManager::WriteCmd( std::string text, int color )
+{
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(m_hstdout, &csbi);
+	SetConsoleTextAttribute(m_hstdout, color);
+	std::cout << text;
+	SetConsoleTextAttribute(m_hstdout, csbi.wAttributes);
+}
+
+bool LogManager::WriteFile( std::string text )
+{
+	return false;
+}
+
+std::string LogManager::GenerateFileName()
+{
+	time_t t = time(0);
+	struct tm* now = localtime(&t);
+
+	std::stringstream str;
+	str << (now->tm_year+1900) << "-"
+		<< (now->tm_mon+1) << "-"
+		<< now->tm_mday << " "
+		<< now->tm_hour << "-"
+		<< now->tm_min << "-"
+		<< now->tm_sec << ".log";
+
+	return str.str();
+}
+
+std::string LogManager::GetTimestamp()
+{
+	time_t t = time(0);
+	struct tm* now = localtime(&t);
+
+	std::stringstream str;
+	str << "[" << now->tm_hour << ":"
+		<< now->tm_min << ":"
+		<< now->tm_sec << "]";
+
+	return str.str();
+}
+
+void LogManager::VerifyLogPath()
+{
+	char* pathbuf = _getcwd(NULL, 0);
+	std::string logPath(pathbuf);
+	logPath += m_logFilePath;
+
+	DWORD fileAttrib = GetFileAttributesA(logPath.c_str());
+	if(fileAttrib & FILE_ATTRIBUTE_DIRECTORY)
+		return;
+	else if(fileAttrib == INVALID_FILE_ATTRIBUTES)
+	{
+		mkdir(logPath.c_str());
+	}
+	else
+	{
+		bDisableFile = true;
+		WriteCmd("Couldn't verify/create log path; disabling logging to file.", Log::COLOR_WARNING);
+	}
+}
+
+bool LogManager::OpenFile()
+{
+	return false;
+}
+
+void LogManager::CloseFile()
+{
+}
+
+
+bool Log::Write( std::string text, int color /*= COLOR_DEFAULT*/ )
+{
+	return LogManager::GetInstance().Write(text,color);
+}
+
+bool Log::Writeln( std::string text, int color /*= COLOR_DEFAULT*/ )
+{
+	text += "\n";
+	return Log::Write(text, color);
+}
+
+bool Log::Warn( std::string warn )
+{
+	return Log::Writeln(warn, Log::COLOR_WARNING);
+}
+
+bool Log::Err( std::string err )
+{
+	return Log::Writeln(err, Log::COLOR_ERROR);
+}
---
+++ LogManager.h
@@ -0,0 +1,70 @@
+#ifndef LOGGING_H
+#define LOGGING_H
+
+#include <string>
+#include <fstream>
+#include <Windows.h>
+
+namespace Log {
+
+	enum {
+		COLOR_BLACK,
+		COLOR_BLUE,
+		COLOR_GREEN,
+		COLOR_AQUA,
+		COLOR_RED,
+		COLOR_PURPLE,
+		COLOR_YELLOW,
+		COLOR_WHITE,
+		COLOR_GRAY,
+		COLOR_LIGHT_BLUE,
+		COLOR_LIGHT_GREEN,
+		COLOR_LIGHT_AQUA,
+		COLOR_LIGHT_RED,
+		COLOR_LIGHT_PURPLE,
+		COLOR_LIGHT_YELLOW,
+		COLOR_LIGHT_WHITE
+	};
+
+	const static int COLOR_DEFAULT	= COLOR_LIGHT_WHITE;
+	const static int COLOR_ERROR	= COLOR_LIGHT_RED;
+	const static int COLOR_WARNING	= COLOR_LIGHT_YELLOW;
+
+	bool Write(std::string text, int color = Log::COLOR_DEFAULT);
+	bool Writeln(std::string text, int color = Log::COLOR_DEFAULT);
+	bool Warn(std::string warn);
+	bool Err(std::string err);
+}
+
+class LogManager {
+
+	public:
+		~LogManager();
+
+		static LogManager& GetInstance(){ return m_instance; }
+
+		bool Write(std::string text, int color);
+
+
+	private:
+		LogManager();
+
+		std::string m_logFilePath;
+		std::string m_logFileName;
+		std::ofstream* m_logFile;
+		HANDLE m_hstdout;
+		bool bDisableFile;
+
+		static LogManager m_instance;
+
+		void WriteCmd(std::string text, int color);
+		bool WriteFile(std::string text);
+
+		std::string GenerateFileName();
+		std::string GetTimestamp();
+		bool OpenFile();
+		void CloseFile();
+		void VerifyLogPath();
+};
+
+#endif
